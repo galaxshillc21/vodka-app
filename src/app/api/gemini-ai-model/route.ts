09f303749c6d17736f5015f343b2ccfcb8c69afa
galaxshillc <120360791+galaxshillc@users.noreply.github.com>
2025-07-21 10:54:06 +0100

    Adding .nvmrc and fixing ai route.ts

--- src/app/api/gemini-ai-model/route.ts
+++ src/app/api/gemini-ai-model/route.ts
@@ -1,5 +1,4 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { NextResponse } from "next/server";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || "");
 const model = genAI.getGenerativeModel({
