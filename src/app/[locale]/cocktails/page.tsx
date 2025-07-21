e35fad528648d873faa3dbdf0d763ad352170436
galaxshillc <120360791+galaxshillc@users.noreply.github.com>
2025-07-21 11:05:00 +0100

    Adding base url

--- src/app/[locale]/cocktails/page.tsx
+++ src/app/[locale]/cocktails/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import { useTranslations } from "next-intl"; // Import useTranslations
 
-const baseUrl = "http://localhost:3000"; // or your hosted domain
+const baseUrl = process.env.BASE_URL; // or your hosted domain
 
 /**
  * Fetches the response from the Gemini AI model API.
